05642f0c63cb4bd68a26e836c56499efde9ac1d8
Jhon Cortes <jhone.cortesg@gmail.com>
2013-06-08 13:15:28 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalis::Application.config.secret_token = '3171c2bf7178d08dc97da41039025c65688f9af30ec540da26ab39ea11c4f838ef66286b2b8809e06cf4b3a037cf309f4f5b3ccd900f0d37644da088f2255a9f'
