a5eada2bbf3257d3d923b705b9629894acba533c
jose david lombana <jose4125@gmail.com>
2013-06-08 20:08:46 -0500

    algo de estilos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Personalis::Application.routes.draw do
   
 
+
+
+  resources :profiles
+
+
   # ROUTES FOR DEVISE
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},controllers: {omniauth_callbacks: "omniauth_callbacks"}
   
